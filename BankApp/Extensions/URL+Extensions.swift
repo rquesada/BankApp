050b1f672582c977f782a48ab79d7edb7b0e374b
Roy Quesada <royquesada@Roys-MacBook-Pro.local>
2023-03-26 16:28:28 -0600

    init project

---
+++ BankApp/Extensions/URL+Extensions.swift
@@ -0,0 +1,15 @@
+
+
+
+import Foundation
+
+extension URL {
+    
+    static func urlForAccounts() -> URL? {
+         return URL(string: "https://coral-stealth-anglerfish.glitch.me/api/accounts")
+    }
+    
+    static func urlForCreateAccounts() -> URL? {
+        return URL(string: "https://coral-stealth-anglerfish.glitch.me/api/accounts")
+    }
+}
